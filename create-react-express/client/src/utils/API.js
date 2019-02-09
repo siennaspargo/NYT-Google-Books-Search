06cf71a403da89f2893e5922724ef6c706d900cf
SiennaS <ssienna48@gmail.com>
2019-02-09 11:27:43 -0500

    getting there

---
+++ create-react-express/client/src/utils/API.js
@@ -0,0 +1,30 @@
+// front end api calls to database and google books API
+
+import axios from "axios";
+
+export default {
+// call to google books API
+getBookTitle: function(searchedTitle) {
+  return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + searchedTitle);
+},
+
+
+
+
+
+
+
+
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const router = router();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +19,10 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
+app.use(router);
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
