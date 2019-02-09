1b630abe9a07adcbafd7d7453e4b3bb910aa8eb2
SiennaS <ssienna48@gmail.com>
2019-02-09 13:47:10 -0500

    files better

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const router = router();
+const router = require("./routing")
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
